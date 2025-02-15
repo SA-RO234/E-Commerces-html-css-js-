482c3f670d2b8810fb07116d32dc2b3d3648fb18
Rosa <rozasun61@gmail.com>
2025-02-15 15:54:00 +0700

    Fixed Latest

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,54 @@
 import React from 'react'
 
-export const navbar = () => {
+export function Navbar(){
   return (
-    <div>navbar</div>
-  )
+    <div>
+      <div class="navbar-left">
+        <button type="button" id="Menu">
+          <img src="/assets/menu.png" alt="" />
+        </button>
+        <div class="line"></div>
+        <a href="/ListCart.html">
+          <img src="/assets/Buy.png" alt="" />
+        </a>
+        <span id="cartindex-QTY">0</span>
+      </div>
+      <div class="navList" id="navList">
+        <div class="item" id="fetch-Winter">
+          <img src="/assets/menuIcon/menu1.png" alt="" />
+          <h1>Winter clothing</h1>
+        </div>
+        <div class="item" id="Trousers-jeans">
+          <img src="/assets/menuIcon/menu2.png" alt="" />
+          <h1>Trousers & jeans</h1>
+        </div>
+        <div class="item" id="Shoes-sandle">
+          <img src="/assets/menuIcon/menu3.png" alt="" />
+          <h1>Shoes & sandle</h1>
+        </div>
+        <div class="item" id="Hats-caps">
+          <img src="/assets/menuIcon/menu4.png" alt="" />
+          <h1>Hats & caps</h1>
+        </div>
+        <div class="item" id="Bags-purses">
+          <img src="/assets/menuIcon/menu5.png" alt="" />
+          <h1>Bags & purses</h1>
+        </div>
+        <div class="item" id="Ladies-Whatches">
+          <img src="/assets/menuIcon/menu6.png" alt="" />
+          <h1>Ladies Whatches</h1>
+        </div>
+      </div>
+      <div class="nabar-right">
+        <button type="button">
+          <i class="fa-solid fa-chevron-left"></i>
+        </button>
+        <button type="button">
+          <i class="fa-solid fa-chevron-right"></i>
+        </button>
+      </div>
+    </div>
+  );
 }
+
+export default Navbar;
